e8bc678a7c05ae100f890b6089e99e990b6a3b93
Mark Rotteveel <mark@lawinegevaar.nl>
2018-10-13 15:38:09 +0200

    Simplify views query a bit and fix syntax error

--- catalog/queries/RetrieveViewsQuery.fb25.sql
+++ catalog/queries/RetrieveViewsQuery.fb25.sql
@@ -8,25 +8,8 @@ select
 from (
   select 
     RDB$RELATION_NAME as TABLE_NAME,
-    case
-      when RDB$RELATION_TYPE = 0 or RDB$RELATION_TYPE is null and RDB$VIEW_BLR is null 
-        then case 
-          when RDB$SYSTEM_FLAG = 1 
-            then 'SYSTEM TABLE' 
-          else 'TABLE' 
-        end
-      when RDB$RELATION_TYPE = 1 or RDB$RELATION_TYPE is null and RDB$VIEW_BLR is not null 
-        then 'VIEW'
-      when RDB$RELATION_TYPE = 2 
-        then 'TABLE' -- external table
-      when RDB$RELATION_TYPE = 3 
-        then 'SYSTEM TABLE'
-      when RDB$RELATION_TYPE in (4, 5) 
-        then 'GLOBAL TEMPORARY'
-      else 'TABLE' -- assume default, should not occur though
-    end as TABLE_TYPE,
-    RDB$VIEW_SOURCE VIEW_SOURCE
+    RDB$VIEW_SOURCE VIEW_SOURCE,
     RDB$DESCRIPTION as COMMENTS
   from RDB$RELATIONS
+  where RDB$RELATION_TYPE = 1 or RDB$RELATION_TYPE is null and RDB$VIEW_BLR is not null
 ) RELATIONS
-where TABLE_TYPE = 'VIEW'
